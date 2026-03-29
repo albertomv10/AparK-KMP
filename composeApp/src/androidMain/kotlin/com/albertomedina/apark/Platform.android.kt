6bdbbe2b5086d81e92dc5f12f0040437f2eeef02
Alberto Medina <albertomedinavaquero@gmail.com>
2026-03-29 12:19:54 +0200

    Eliminación de las implementaciones de plataforma específicas para Android e iOS.
    - Eliminados los archivos `Platform.android.kt` y `Platform.ios.kt`.
    - Eliminada la lógica para obtener el nombre y versión del sistema operativo a través de la interfaz `Platform` y la función `getPlatform`.

--- composeApp/src/androidMain/kotlin/com/albertomedina/apark/Platform.android.kt
+++
@@ -1,9 +0,0 @@
-package com.albertomedina.apark
-
-import android.os.Build
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
